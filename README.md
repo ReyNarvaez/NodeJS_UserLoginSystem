ffe79b493508402b96d57b78e91a44b06b8e5421
reynarvaez <reynaldo_narvaez@hotmail.com>
2018-04-07 19:22:32 -0400

    added all middleware dependencies and modified layout

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ User Login System Implemented in NodeJS
 * node 8.2.1
 * express 4.15.5
 * pug 2.0.3
+* bootstrap 4.0
 * mongoDB 3.6.3
 * passport 0.4.0
+* multer 1.3.0
--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var expressValidator = require('express-validator');
 var session = require('express-session');
 var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var multer = require('multer');
-var flash = require('connect-flash');
+var upload = multer({dest: './uploads'});
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
@@ -22,13 +23,46 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(multer({dest:'./uploads'}));
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Handle Sessions
+app.use(session({
+	secret:'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value){
+		var namespace = param.split('.')
+		, root = namespace.shift()
+		, formParam = root;
+
+		while(namespace.length){
+			formParam += '[' + namespace.shift() + ']';
+		}
+
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+}));
+
+app.use(require('connect-flash')());
+app.use(function(req,res,next){
+	res.locals.messages = require('express-messages')(req,res);
+	next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
