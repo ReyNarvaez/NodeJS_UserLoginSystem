20320ee2b18b81d8765d91cde4d94530470b96c2
reynarvaez <reynaldo_narvaez@hotmail.com>
2018-04-09 16:55:24 -0400

    register route multipart form handling, validation and error rendering

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,68 @@
 var express = require('express');
 var router = express.Router();
 
+//MULTIPART FORM PARSE
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Register' });
+  res.render('register', { title: 'Register', form: {} });
 });
 
-router.post('/register', function(req, res, next) {
-	console.log("submited register");
+router.post('/register', upload.single('profileImage'), function(req, res, next) {
+	
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+	
+	if(req.file){
+
+		console.log('File Uploaded');
+
+		var profileImage = req.file.filename;
+	}
+	else{
+		console.log('No File Attached');
+
+		var profileImage = "noimage.jpg";
+	}
+
+	//Form Validator
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('email','Email field is required').notEmpty();
+	req.checkBody('email','Email is not valid').isEmail();
+	req.checkBody('username','Username field is required').notEmpty();
+	req.checkBody('password','Password field is required').notEmpty();
+	req.checkBody('password2','Confirm Password field is required').notEmpty();
+	req.checkBody('password2','Passwords do not match').equals(req.body.password);
+
+	//Check Errors
+	var errors = req.validationErrors();
+
+	if(errors){
+
+		var form = {
+			name: name,
+			email: email,
+			username: username
+		};
+
+		res.render('register',{
+			errors: errors,
+			form, form
+		});
+	}
+	else{
+		console.log('No Errors');
+	}
+
 });
 
 router.get('/login', function(req, res, next) {
@@ -19,7 +70,8 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-	console.log("submited login");
+
 });
 
 module.exports = router;
+		 
